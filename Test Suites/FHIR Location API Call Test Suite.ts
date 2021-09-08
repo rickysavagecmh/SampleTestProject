1f750af1e075756e2c20ec74f73b9a9c6a2eee93
NRGIT <NRGIT@10.0.0.51>
2021-09-08 04:50:37 -0500

    Added FHIR Location Test Suite

---
+++ Test Suites/FHIR Location API Call Test Suite.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>FHIR Location API Call Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>c8fc1d34-2879-4e7b-bf04-7dc1de980426</testSuiteGuid>
+   <testCaseLink>
+      <guid>1a73f0f5-56f8-40af-afb9-9b393c3612aa</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/FHIR Location Sample Test Case</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
